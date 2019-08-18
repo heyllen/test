b1198afdb91ae7c53e91bbfac11f91e63ac6e4c6
heyllen <393286997@qq.com>
2019-08-18 13:51:48 +0800

    测试提交的版本

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 /**
  * Created by Administrator on 2019/8/11.
  */
 public class Main {
     public static void main(String args[])
     {
-        System.out.println("1111");
+        try {
+            URL u = null;
+            u = new URL("file:/d:/my/lib/");
+            URLClassLoader ucl = new URLClassLoader(new URL[]{ u }) ;
+            Class c = ucl.loadClass(args[0]) ;
+            System.out.println("1111");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
